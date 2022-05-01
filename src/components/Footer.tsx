31310795249b298705922e33e8329faffb15bb82
Tatu Pesonen <tatu@narigon.dev>
2022-05-01 03:48:51 +0300

    fix: add view source link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,24 @@
-import { Box, Center, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Text } from "@chakra-ui/react";
+import { SiGithub } from "react-icons/si";
 
 export const Footer = () => {
   return (
     <Center py="2rem" mt="7rem">
-      <Flex alignItems="center" gap="0.5rem" textColor="brand.darkGray">
-        <Text fontWeight="semibold">Typemaster 2021</Text>
-        <Text fontWeight="semibold">|</Text>
-        <Text>All Rights Reserved</Text>
+      <Flex direction="column" gap="1rem">
+        <Flex alignItems="center" gap="0.5rem" textColor="brand.darkGray">
+          <Text fontWeight="semibold">Typemaster 2021</Text>
+          <Text fontWeight="semibold">|</Text>
+          <Text>All Rights Reserved</Text>
+        </Flex>
+        <Button
+          as="a"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/tatupesonen/typemaster-keyboard-challenge"
+          leftIcon={<SiGithub />}
+        >
+          View source
+        </Button>
       </Flex>
     </Center>
   );
